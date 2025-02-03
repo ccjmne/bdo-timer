816b6edf7de4c5b4276c617b6c05cf3d265ddcbc
Éric NICOLAS <ccjmne@gmail.com>
2025-02-03 23:02:46 +0100

    Version 0.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let cur = 0;
+  let max = 10;
+  let interval: number | null = null;
+  let endSound: HTMLAudioElement;
+
+  function startStop() {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    } else {
+      if (cur === max) {
+        cur = 0;
+      }
+
+      interval = setInterval(() => {
+        if (cur >= max) {
+          clearInterval(interval!);
+          interval = null;
+          endSound.play();
+        } else {
+          cur += 1;
+        }
+      }, 100);
+    }
+  }
+
+  onMount(() => {
+    endSound = document.querySelector('#endSound')!;
+  });
+</script>
+
+<main>
+<form on:submit|preventDefault={startStop}>
+  <input type="number" bind:value={cur} min="0" max={max} />
+  /
+  <input type="number" bind:value={max} min="0" />
+  <button type="submit">{interval ? 'Stop' : 'Start'}</button>
+</form>
+<input type="range" bind:value={cur} min="0" max={max} />
+</main>
+<audio id="endSound" src="./beep.ogg"></audio>
+
+<style lang="scss">
+main {
+    display: flex;
+    place-content: center;
+    background: #333;
+    color: ghostwhite;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+:root * {
+    font-size: 24px;
+}
+
+input[type="number"] {
+    border: none;
+    background: transparent;
+    outline: none;
+    color: inherit;
+    width: 4ch;
+}
+
+input[type="range"] {
+    width: 90vw;
+}
+</style>
+
