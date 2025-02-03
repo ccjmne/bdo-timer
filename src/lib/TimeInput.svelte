10e090aca7226f941ff80e058578dd50f222ddb6
Éric NICOLAS <ccjmne@gmail.com>
2025-02-04 00:12:37 +0100

    Increase/decrease with mousewheel

--- src/lib/TimeInput.svelte
+++ src/lib/TimeInput.svelte
@@ -6,6 +6,9 @@
   let { value = $bindable(100) } = $props()
 
   $effect(() => {
+    if (value < 0) {
+      value = 0
+    }
     hours = Math.floor(value / 3600)
     minutes = Math.floor((value % 3600) / 60)
     seconds = value % 60
@@ -13,14 +16,18 @@
   $effect(() => {
     value = hours * 3600 + minutes * 60 + seconds
   })
+
+  function hover({ target }: MouseEvent) {
+    (target as HTMLInputElement).focus()
+  }
 </script>
 
 <main>
-  <input type="number" bind:value={hours} min="0" max="23" />
+  <input type="number" bind:value={hours} onfocus={() => {}} onmouseover={hover} onwheel={({ deltaY, ...e }) => hours = Math.max(0, hours - Math.sign(deltaY))} min="0" max="23" />
   <span>:</span>
-  <input type="number" bind:value={minutes} min="0" max="59" />
+  <input type="number" bind:value={minutes} onfocus={() => {}} onmouseover={hover} onwheel={({ deltaY, ...e }) => minutes = Math.max(0, minutes - Math.sign(deltaY))} min="0" max="59" />
   <span>:</span>
-  <input type="number" bind:value={seconds} min="0" max="59" />
+  <input type="number" bind:value={seconds} onfocus={() => {}} onmouseover={hover} onwheel={({ deltaY, ...e }) => seconds = Math.max(0, seconds - Math.sign(deltaY))} min="0" max="59" />
   <small>hours</small>
   <small></small>
   <small>minutes</small>
