e7f12c9c0ca44ab381b5a732a307e251fb9c4f06
Éric NICOLAS <ccjmne@gmail.com>
2025-02-04 19:38:13 +0100

    Add 'live' pulsating status bullet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
   let running = $derived(interval !== null)
   let action = $derived(running ? 'Pause' : { [max]: 'Restart', 0: 'Start' }[cur] || 'Resume')
   let cssdir = $derived(dir ? 'direction: ltr' : 'direction: rtl')
+  let status = $derived(running ? 'status status-accent' : 'status')
 
   function resume() {
     clearInterval(interval!)
@@ -66,7 +67,11 @@
   </div>
   <input type="range" bind:value={cur} min={0} {max} class="range w-full" style={cssdir} />
   <div class="flex flex-col gap-2 sm:flex-row place-items-center w-full justify-between">
-    <fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
+    <fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box relative">
+      {#if running}
+        <div class="absolute right-1.5 top-0.5 {status} animate-ping"></div>
+      {/if}
+      <div class="absolute right-1.5 top-0.5 {status}"></div>
       {#if dir}
         <legend class="fieldset-legend h-0">Elapsed</legend>
         <TimeInput bind:value={() => cur, v => ((max = Math.max(max, v)), (cur = v))} />
