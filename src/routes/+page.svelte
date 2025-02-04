cf4b00011df88203a499e308fff0fcafb3e96593
Éric NICOLAS <ccjmne@gmail.com>
2025-02-04 05:51:44 +0100

    Add ability to count up or down

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,12 @@
   let interval: number | null = $state(null)
   let endSound: HTMLAudioElement
 
+  let loop = $state(false)
+  let dir = $state(true)
+
   let running = $derived(interval !== null)
   let action = $derived(running ? 'Pause' : { 0: 'Start', [max]: 'Restart' }[cur] || 'Resume')
+  let cssdir = $derived(dir ? 'direction: ltr' : 'direction: rtl')
 
   function resume() {
     clearInterval(interval!)
@@ -42,22 +46,30 @@
   onMount(() => {
     endSound = document.querySelector('#endSound')!
   })
-
-  let loop = $state(false)
 </script>
 
 <main class="flex flex-col items-center justify-center h-screen bg-gray-800 text-white gap-4">
-  <div style="font-size: .5em">
+  <div id="controls" class="w-50">
     <label class="fieldset-label">
       <input type="checkbox" bind:checked={loop} class="toggle toggle-xs" />
       Loop: {loop ? 'on' : 'off'}
     </label>
+    <label class="fieldset-label">
+      <input type="checkbox" bind:checked={dir} class="toggle toggle-xs" />
+      Count: {dir ? 'up' : 'down'}
+    </label>
   </div>
-  <input type="range" bind:value={cur} min="0" {max} class="range" />
+  <input type="range" bind:value={cur} min={0} {max} class="range" style={cssdir} />
   <div id="textual">
-    <TimeInput bind:value={() => cur, v => ((cur = v), (max = Math.max(max, cur)))} />
-    <span class="muted">of</span>
-    <TimeInput bind:value={() => max, v => ((max = v), (cur = Math.min(max, cur)))} />
+    {#if dir}
+      <TimeInput bind:value={() => cur, v => ((max = Math.max(max, v)), (cur = v))} />
+      <span class="muted">to</span>
+      <TimeInput bind:value={() => max, v => ((max = v), (cur = Math.min(v, cur)))} />
+    {:else}
+      <TimeInput bind:value={() => max - cur, v => ((max = Math.max(max, v)), (cur = max - v))} />
+      <span class="muted">from</span>
+      <TimeInput bind:value={() => max, v => ((cur = Math.max(0, cur + v - max)), (max = v))} />
+    {/if}
   </div>
   <button type="button" class="btn btn-primary w-50" onclick={click}>{action}</button>
 </main>
@@ -68,5 +80,12 @@
     display: flex;
     align-items: center;
     gap: 2em;
+  }
+
+  #controls {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 1em;
+    font-size: 0.5em;
   }
 </style>
