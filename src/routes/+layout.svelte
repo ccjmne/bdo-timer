93109bffa49b11637e123454607e262ca2e6c3eb
Éric NICOLAS <ccjmne@gmail.com>
2025-02-06 00:16:54 +0100

    Preload fonts for EditableNumber

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import '../app.css'
 </script>
 
+<svelte:head>
+  <link rel="preload" href="/outfit.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+  <link rel="preload" href="/syne.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+</svelte:head>
+
 <main class="flex w-screen h-screen place-items-center place-content-center bg-gray-800">
   <div class="w-[90vw] sm:w-2xs">
     <slot />
