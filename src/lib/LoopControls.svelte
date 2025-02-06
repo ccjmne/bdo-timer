fac128940f5a3c04b255ac970f5077a7cc0b4114
Éric NICOLAS <ccjmne@gmail.com>
2025-02-06 02:57:25 +0100

    Introduce THIRD-PARTY-LICENSES

--- src/lib/LoopControls.svelte
+++ src/lib/LoopControls.svelte
@@ -22,29 +22,6 @@
       class="btn join-item"
       onclick={() => (loop[1] = last ? Infinity : loop[0])}
     >
-      <!--
-The SVG icons in this file were obtained from https://tabler.io/icons.
-
-                                  MIT License
-
-                       Copyright (c) 2020-2024 Paweł Kuna
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. -->
       <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
         <path
           fill="none"
