cfa821489ca87eded575effbb44923d94fabdc25
Éric NICOLAS <ccjmne@gmail.com>
2025-02-10 00:53:24 +0100

    Prefer plain grep over rg for CI/CD

--- tools/prepare-fonts.sh
+++ tools/prepare-fonts.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # Get only the following glyphs: 0123456789+-,.
-readonly outfit=$(curl -L 'https://fonts.googleapis.com/css2?family=Outfit:wght@400&text=0123456789%2B-%2C.' | rg --pcre2 --no-line-number --no-column --only-matching '(?<=url\()[^)]+')
+readonly outfit=$(curl -L 'https://fonts.googleapis.com/css2?family=Outfit:wght@400&text=0123456789%2B-%2C.' | grep -Po '(?<=url\()[^)]+')
 
 # Get only the following glyphs: ∞
-readonly syne=$(curl -L 'https://fonts.googleapis.com/css2?family=Syne:wght@500&text=%E2%88%9E' | rg --pcre2 --no-line-number --no-column --only-matching '(?<=url\()[^)]+')
+readonly syne=$(curl -L 'https://fonts.googleapis.com/css2?family=Syne:wght@500&text=%E2%88%9E' | grep -Po '(?<=url\()[^)]+')
 
 curl -L "$outfit" -o outfit.ttf
 curl -L "$syne" -o syne.ttf
